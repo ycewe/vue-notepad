f037c6679a440ebc5d818c52c9195b315ea6c435
Eric Ewe <eric.ewe@zalora.com>
2016-07-14 15:32:58 +0800

    enable note creation

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,31 @@
 <template>
-  <div id="app">
+  <div>
+    <create-note-form></create-note-form>
     <notes></notes>
   </div>
 </template>
 
 <script>
-import Notes from './components/notes/Index.vue';
+  import Notes from './components/notes/Index.vue';
+  import CreateNoteForm from './components/notes/Create.vue';
 
-export default {
-  components: {
-    Notes,
-  },
-};
+  export default {
+    components: {
+      Notes,
+      CreateNoteForm,
+    },
+  };
 </script>
 
 <style>
-html {
-  height: 100%;
-}
-
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
-
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
-
-#app a {
-  color: #42b983;
-  text-decoration: none;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
-}
+  html {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: sans-serif;
+  }
+  body {
+    background: #eee;
+    padding: 0 16px;
+  }
 </style>
--- src/components/notes/Index.vue
+++ src/components/notes/Index.vue
@@ -1,42 +1,39 @@
 <template>
-  <ol>
-    <li v-for="note in notes">
-      <pre>
-        {{note | json}}
-      </pre>
-    </li>
-  </ol>
+  <div class="notes">
+    <note v-for="note in notes" :note="note"></note>
+  </div>
 </template>
 <script>
-import firebase from 'firebase';
+  import firebase from 'firebase';
+  import Note from './Note';
 
-export default {
-  data() {
-    return {
-      notes: [],
-    };
-  },
-  ready() {
-    const config = {
-      apiKey: 'AIzaSyB31zKN-7XIwpTwFzgkEY-kFrXqP72n1Ao',
-      authDomain: 'alexander-b2c0d.firebaseapp.com',
-      databaseURL: 'https://alexander-b2c0d.firebaseio.com',
-      storageBucket: 'alexander-b2c0d.appspot.com',
-    };
-    firebase.initializeApp(config);
+  export default {
+    components: {
+      Note,
+    },
+    data() {
+      return {
+        notes: [],
+      };
+    },
+    ready() {
+      const config = {
+        apiKey: 'AIzaSyB31zKN-7XIwpTwFzgkEY-kFrXqP72n1Ao',
+        authDomain: 'alexander-b2c0d.firebaseapp.com',
+        databaseURL: 'https://alexander-b2c0d.firebaseio.com',
+        storageBucket: 'alexander-b2c0d.appspot.com',
+      };
+      firebase.initializeApp(config);
 
-    /** example
-    firebase.database().ref('notes').set([
-      { title: 'Hello world!', content: 'Lorem Ipsum' },
-    ]); **/
-
-    firebase.database().ref('notes').on('child_added', (snapshot) => {
-      const note = snapshot.val();
-      this.notes.unshift(note);
-    });
-  },
-};
+      firebase.database().ref('notes').on('child_added', (snapshot) => {
+        const note = snapshot.val();
+        this.notes.unshift(note);
+      });
+    },
+  };
 </script>
 <style>
-
+  .notes {
+    padding: 0 100px;
+  }
 </style>
